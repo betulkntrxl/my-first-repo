960a175f846ea5e4263e18abb214b1f13b4863d0
restrella3 <126694387+restrella3@users.noreply.github.com>
2023-06-05 18:09:30 -0700

    added tests for sliders

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,9 +6,25 @@ import App from './App';
 describe('testing the App', () => {
   afterEach(cleanup);
 
-  it('renders a text', () => {
+  it('renders a menu', () => {
     render(<App />);
-    const text = screen.getByLabelText('menu');
-    expect(text).toBeTruthy();
+    const menu = screen.getByLabelText('menu');
+    expect(menu).toBeTruthy();
+  });
+
+  it('renders a Temperature slider', () => {
+    render(<App />);
+    const temperature = screen.getByLabelText('Temperature');
+    expect(temperature).toBeTruthy();
+  });
+  it('renders a Top P slider', () => {
+    render(<App />);
+    const topP = screen.getByLabelText('Top P');
+    expect(topP).toBeTruthy();
+  });
+  it('renders a Max Tokens slider', () => {
+    render(<App />);
+    const maxTokens = screen.getByLabelText('Max Tokens');
+    expect(maxTokens).toBeTruthy();
   });
 });
