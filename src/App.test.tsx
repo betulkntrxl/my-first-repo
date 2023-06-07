22c46165c6d859903d94283088d1147af07df3a5
restrella3 <126694387+restrella3@users.noreply.github.com>
2023-06-06 19:07:01 -0700

    add test for menu

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -12,6 +12,13 @@ describe('testing the App', () => {
     const menu = screen.getByLabelText('menu');
     expect(menu).toBeTruthy();
   });
+  it('opens a menu', async () => {
+    render(<App />);
+    const user = userEvent.setup();
+    const menuElement = screen.getByLabelText('menu');
+    await user.click(menuElement);
+    expect(menuElement).toBeTruthy();
+  });
 
   it('renders a Temperature slider', () => {
     render(<App />);
