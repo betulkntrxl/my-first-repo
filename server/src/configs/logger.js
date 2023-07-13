7190bb4c7c4167e527b03f994048d13b9757ecb4
Liam O'Brien <liam.obrien@mckesson.com>
2023-07-13 13:30:25 +0100

    Making improvements to error handling and logging in the server

--- server/src/configs/logger.js
+++ server/src/configs/logger.js
@@ -1,7 +1,16 @@
 import winston from 'winston';
 
+const { combine, timestamp, printf, colorize, align } = winston.format;
+
 export const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'debug',
-  format: winston.format.json(),
-  transports: [new winston.transports.Console({ format: winston.format.simple() })],
+  format: combine(
+    colorize({ all: true }),
+    timestamp({
+      format: 'YYYY-MM-DD hh:mm:ss.SSS Z',
+    }),
+    align(),
+    printf(info => `[${info.timestamp}] ${info.level}: ${info.message}`)
+  ),
+  transports: [new winston.transports.Console()],
 });
