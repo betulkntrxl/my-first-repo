4d02f0b034517b68066dc9d9a79207294aa4b8f5
Robert Estrella <robert.estrella@mckesson.com>
2023-07-26 07:10:00 -0700

    mock api tests for status 500, 401 and maxtokens

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { render, cleanup, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import { bool } from 'prop-types';
-
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import App from './App';
@@ -44,7 +42,7 @@ afterAll(() => server.close());
 describe('testing the App', () => {
   afterEach(cleanup);
 
-  it('sends a message', async () => {
+  it('sends a message and returns status 200 ok', async () => {
     render(<App />);
     const user = userEvent.setup();
     const sendmessageElement = screen.getByTitle('sendmessage');
@@ -54,7 +52,81 @@ describe('testing the App', () => {
     await user.click(sendElement);
 
     expect(sendElement).toBeTruthy();
-  }, 20000);
+  }, 5000);
+
+  it('sends a message and returns API error', async () => {
+    rest.post('/api/prompt', (req, res, ctx) =>
+      res(ctx.json({ errorMessage: 'an error has occured' }), ctx.status(500))
+    );
+
+    render(<App />);
+    const user = userEvent.setup();
+    const sendmessageElement = screen.getByTitle('sendmessage');
+    await user.click(sendmessageElement);
+    await user.keyboard('hello');
+    const sendElement = screen.getByTitle('send');
+    await user.click(sendElement);
+
+    expect(sendElement).toBeTruthy();
+  }, 5000);
+
+  it('sends a message and returns status 401 Unauthorized error', async () => {
+    rest.post('/api/prompt', (req, res, ctx) =>
+      res(
+        ctx.json({ errorMessage: 'User is not logged in, authenticate path is /api/auth/login' }),
+        ctx.status(401)
+      )
+    );
+
+    render(<App />);
+    const user = userEvent.setup();
+    const sendmessageElement = screen.getByTitle('sendmessage');
+    await user.click(sendmessageElement);
+    await user.keyboard('hello');
+    const sendElement = screen.getByTitle('send');
+    await user.click(sendElement);
+
+    expect(sendElement).toBeTruthy();
+  }, 5000);
+
+  it('sends a message and returns total tokens exceeding max token limit', async () => {
+    rest.post('/api/prompt', (req, res, ctx) =>
+      res(
+        ctx.json({
+          id: 'chatcmpl-7gQM4JDiQa2Dc4dErFzWLnTfD0dYR',
+          object: 'chat.completion',
+          created: 1690345440,
+          model: 'gpt-35-turbo',
+          choices: [
+            {
+              index: 0,
+              finish_reason: 'stop',
+              message: {
+                role: 'assistant',
+                content: 'Hello! How can I assist you today?',
+              },
+            },
+          ],
+          usage: {
+            completion_tokens: 9,
+            prompt_tokens: 25,
+            total_tokens: 340,
+          },
+        }),
+        ctx.status(401)
+      )
+    );
+
+    render(<App />);
+    const user = userEvent.setup();
+    const sendmessageElement = screen.getByTitle('sendmessage');
+    await user.click(sendmessageElement);
+    await user.keyboard('hello');
+    const sendElement = screen.getByTitle('send');
+    await user.click(sendElement);
+
+    expect(sendElement).toBeTruthy();
+  }, 5000);
 
   it('renders a menu', async () => {
     render(<App />);
