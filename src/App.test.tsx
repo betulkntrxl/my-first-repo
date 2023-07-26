051a6f27855f7c2967a85331be43fd8858319ae4
Robert Estrella <robert.estrella@mckesson.com>
2023-07-25 21:40:29 -0700

    mock api tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,9 +3,43 @@ import { render, cleanup, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { bool } from 'prop-types';
+
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import App from './App';
 
-import Home from './pages/Home/Home';
+const server = setupServer(
+  rest.post('/api/prompt', (req, res, ctx) =>
+    res(
+      ctx.json({
+        id: 'chatcmpl-7gQM4JDiQa2Dc4dErFzWLnTfD0dYR',
+        object: 'chat.completion',
+        created: 1690345440,
+        model: 'gpt-35-turbo',
+        choices: [
+          {
+            index: 0,
+            finish_reason: 'stop',
+            message: {
+              role: 'assistant',
+              content: 'Hello! How can I assist you today?',
+            },
+          },
+        ],
+        usage: {
+          completion_tokens: 9,
+          prompt_tokens: 25,
+          total_tokens: 34,
+        },
+      })
+    )
+  ),
+  rest.get('/api/version', (req, res, ctx) => res(ctx.json({ greeting: 'hello there' })))
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 describe('testing the App', () => {
   afterEach(cleanup);
