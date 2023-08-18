a642363c689c4e1887a50b52626d245d517b6ef5
Robert Estrella <robert.estrella@mckesson.com>
2023-08-18 13:49:25 -0700

    API timeout test update

--- src/APITimeout.test.tsx
+++ src/APITimeout.test.tsx
@@ -6,8 +6,17 @@ import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import App from './App';
 
+function wait(milliseconds: number | undefined) {
+  return new Promise(resolve => {
+    setTimeout(resolve, milliseconds);
+  });
+}
+
 const server = setupServer(
-  rest.post('/api/prompt', async req => req.passthrough),
+  rest.post('/api/prompt', (req, res, ctx) => {
+    wait(11000);
+    return res(ctx.json({ greeting: "I'm late" }));
+  }),
   rest.get('/api/version', (req, res, ctx) => res(ctx.json({ greeting: 'hello there' }))),
   rest.post('/api/app-insights-event', (req, res, ctx) => res(ctx.status(201))),
   rest.post('/api/app-insights-trace', (req, res, ctx) => res(ctx.status(201))),
@@ -19,22 +28,40 @@ afterAll(() => server.close());
 
 describe('testing the App', () => {
   afterEach(cleanup);
-
+  jest.setTimeout(35000);
   it('sends a message and returns API error', async () => {
-    await act(async () => {
+    act(() => {
       render(<App />);
-      const user = userEvent.setup();
+    });
+    const user = userEvent.setup();
+    await waitFor(() => expect(screen.getByTitle('sendmessage')).toBeVisible()).then(async () => {
       const sendmessageElement = screen.getByTitle('sendmessage');
 
-      await user.click(sendmessageElement);
-      await user.keyboard('hello');
-      const sendElement = screen.getByTitle('send');
-      await user.click(sendElement);
+      fireEvent.click(sendmessageElement);
+      act(() => {
+        fireEvent.change(sendmessageElement, {
+          target: { value: 'hello' },
+        });
+      });
+      // await user.keyboard('hello');
+      await waitFor(() => expect(screen.getByTitle('send')).toBeVisible()).then(async () => {
+        const sendElement = screen.getByTitle('send');
+        act(() => {
+          fireEvent.click(sendElement);
+        });
+        // wait for dialog to be rendered
 
-      // wait for dialog to be rendered
-      await waitFor(() => expect(screen.getByTitle('close-button')).toBeVisible()).then(() => {
-        fireEvent.click(screen.getByTitle('close-button'));
-        expect(sendElement).toBeTruthy();
+        await waitFor(() => expect(screen.getByTitle('close-button')).toBeVisible(), {
+          timeout: 11000,
+        }).then(() => {
+          const closeElement = screen.getByTitle('close-button');
+          act(() => {
+            fireEvent.click(closeElement);
+          });
+          // waitFor(() => expect(screen.getByTitle('sendmessage')).toBeVisible()).then(() => {
+          expect(closeElement).toBeTruthy();
+          // });
+        });
       });
     });
   });
