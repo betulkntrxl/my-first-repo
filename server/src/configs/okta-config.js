9fd037305798fc6834e39226126483a37044cc5d
Liam O'Brien <liam.obrien@mckesson.com>
2023-11-09 13:34:16 +0000

    Defining OIDC scopes depending on McKesson or USON

--- server/src/configs/okta-config.js
+++ server/src/configs/okta-config.js
@@ -6,12 +6,18 @@ const setupOktaConfig = () => {
 
   logger.info('Setting up Okta Config...');
 
+  // We have custom claims / scopes for the McKesson ChatApp deployment to retrieve BU and extra groups
+  const SCOPES =
+    process.env.ORG_DEPLOYMENT === 'uson'
+      ? 'openid profile email'
+      : 'openid profile email ChatApp_groups';
+
   const oidc = new ExpressOIDC({
     issuer: process.env.OKTA_ISSUER,
     client_id: process.env.OKTA_CLIENT_ID,
     client_secret: process.env.OKTA_CLIENT_SECRET,
     appBaseUrl: process.env.OKTA_APP_BASE_URL,
-    scope: 'openid profile email ChatApp_groups',
+    scope: SCOPES,
     routes: {
       login: {
         path: '/api/auth/login',
