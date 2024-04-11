c0f5060000798d77fc5abe3031f768b269492503
Liam O'Brien <liam.obrien@mckesson.com>
2024-04-11 12:00:12 +0100

    Fixing breaking changes upgrading to 3.0.0 of http-proxy-middleware

--- server/src/configs/mule-proxy-config.js
+++ server/src/configs/mule-proxy-config.js
@@ -19,66 +19,68 @@ const setupMulesoftProxyGPT35Turbo = appInsights => {
     target: process.env.MULESOFT_OPENAI_CHAT_API_URL,
     changeOrigin: true,
     pathRewrite: { '^/api/prompt/gpt35turbo': '' },
-    onProxyReq: fixRequestBody,
-    onError: (err, req, res) => {
-      logger.error(`Failed proxying to 3.5 Turbo API... ${err} `);
+    on: {
+      proxyReq: fixRequestBody,
+      error: (err, req, res) => {
+        logger.error(`Failed proxying to 3.5 Turbo API... ${err} `);
 
-      res.writeHead(500, {
-        'Content-Type': 'application/json',
-      });
+        res.writeHead(500, {
+          'Content-Type': 'application/json',
+        });
 
-      res.end(
-        `{ "errorMessage": Sometime went wrong proxying to the 3.5 Turbo API, the service could be busy please try again later.} `,
-      );
+        res.end(
+          `{ "errorMessage": Sometime went wrong proxying to the 3.5 Turbo API, the service could be busy please try again later.} `,
+        );
+      },
+      proxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+        let clientResponse;
+
+        if (proxyRes.statusCode !== 200) {
+          const errorId = uuidv4();
+          logger.error(
+            `Chat 3.5 Turbo API Failed... Error Id ${errorId} HTTP Status Code ${
+              proxyRes.statusCode
+            } HTTP Error Message ${responseBuffer.toString('utf8')} `,
+          );
+
+          if (process.env.DEPLOY_ENVIRONMENT === 'cloud') {
+            appInsights.defaultClient.trackTrace({
+              message: 'ChatApp 3.5 Turbo Prompt API Failed',
+              severity: 3, // Error
+              properties: {
+                chatAppErrorId: errorId,
+                httpStatusCode: proxyRes.statusCode,
+                errorMessage: responseBuffer.toString('utf8'),
+              },
+            });
+          }
+          // Respond with a consistent error message
+          // while also hiding api error details
+          let openAIErrorCode = 'N/A';
+          let openAIErrorMessage =
+            'Unfortunately something went wrong, if you would like to report this issue please include the id of this error.';
+
+          const mulesoftErrorMessage = JSON.parse(responseBuffer.toString('utf8'));
+          if (
+            mulesoftErrorMessage.errorDetails[1] &&
+            mulesoftErrorMessage.errorDetails[1].message &&
+            mulesoftErrorMessage.errorDetails[1].message.error &&
+            mulesoftErrorMessage.errorDetails[1].message.error.code &&
+            mulesoftErrorMessage.errorDetails[1].message.error.message
+          ) {
+            openAIErrorCode = mulesoftErrorMessage.errorDetails[1].message.error.code;
+            openAIErrorMessage = mulesoftErrorMessage.errorDetails[1].message.error.message;
+          }
+          clientResponse = createErrorMessage(errorId, openAIErrorCode, openAIErrorMessage);
+        } else {
+          logger.info(`3.5 Turbo Prompt request successful ${proxyRes.statusCode} `);
+          clientResponse = responseBuffer;
+        }
+
+        return clientResponse;
+      }),
     },
     selfHandleResponse: true,
-    onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
-      let clientResponse;
-
-      if (proxyRes.statusCode !== 200) {
-        const errorId = uuidv4();
-        logger.error(
-          `Chat 3.5 Turbo API Failed... Error Id ${errorId} HTTP Status Code ${
-            proxyRes.statusCode
-          } HTTP Error Message ${responseBuffer.toString('utf8')} `,
-        );
-
-        if (process.env.DEPLOY_ENVIRONMENT === 'cloud') {
-          appInsights.defaultClient.trackTrace({
-            message: 'ChatApp 3.5 Turbo Prompt API Failed',
-            severity: 3, // Error
-            properties: {
-              chatAppErrorId: errorId,
-              httpStatusCode: proxyRes.statusCode,
-              errorMessage: responseBuffer.toString('utf8'),
-            },
-          });
-        }
-        // Respond with a consistent error message
-        // while also hiding api error details
-        let openAIErrorCode = 'N/A';
-        let openAIErrorMessage =
-          'Unfortunately something went wrong, if you would like to report this issue please include the id of this error.';
-
-        const mulesoftErrorMessage = JSON.parse(responseBuffer.toString('utf8'));
-        if (
-          mulesoftErrorMessage.errorDetails[1] &&
-          mulesoftErrorMessage.errorDetails[1].message &&
-          mulesoftErrorMessage.errorDetails[1].message.error &&
-          mulesoftErrorMessage.errorDetails[1].message.error.code &&
-          mulesoftErrorMessage.errorDetails[1].message.error.message
-        ) {
-          openAIErrorCode = mulesoftErrorMessage.errorDetails[1].message.error.code;
-          openAIErrorMessage = mulesoftErrorMessage.errorDetails[1].message.error.message;
-        }
-        clientResponse = createErrorMessage(errorId, openAIErrorCode, openAIErrorMessage);
-      } else {
-        logger.info(`3.5 Turbo Prompt request successful ${proxyRes.statusCode} `);
-        clientResponse = responseBuffer;
-      }
-
-      return clientResponse;
-    }),
     logger: console,
   });
 
@@ -95,66 +97,68 @@ const setupMulesoftProxyGPT4 = appInsights => {
     target: process.env.MULESOFT_OPENAI_CHAT_API_URL_GPT4,
     changeOrigin: true,
     pathRewrite: { '^/api/prompt/gpt4': '' },
-    onProxyReq: fixRequestBody,
-    onError: (err, req, res) => {
-      logger.error(`Failed proxying to GPT 4 API... ${err} `);
+    on: {
+      proxyReq: fixRequestBody,
+      error: (err, req, res) => {
+        logger.error(`Failed proxying to GPT 4 API... ${err} `);
 
-      res.writeHead(500, {
-        'Content-Type': 'application/json',
-      });
+        res.writeHead(500, {
+          'Content-Type': 'application/json',
+        });
 
-      res.end(
-        `{ "errorMessage": Sometime went wrong proxying to the GPT 4 API, the service could be busy please try again later.} `,
-      );
+        res.end(
+          `{ "errorMessage": Sometime went wrong proxying to the GPT 4 API, the service could be busy please try again later.} `,
+        );
+      },
+      proxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+        let clientResponse;
+
+        if (proxyRes.statusCode !== 200) {
+          const errorId = uuidv4();
+          logger.error(
+            `Chat GPT 4 API Failed... Error Id ${errorId} HTTP Status Code ${
+              proxyRes.statusCode
+            } HTTP Error Message ${responseBuffer.toString('utf8')} `,
+          );
+
+          if (process.env.DEPLOY_ENVIRONMENT === 'cloud') {
+            appInsights.defaultClient.trackTrace({
+              message: 'ChatApp GPT 4 Prompt API Failed',
+              severity: 3, // Error
+              properties: {
+                chatAppErrorId: errorId,
+                httpStatusCode: proxyRes.statusCode,
+                errorMessage: responseBuffer.toString('utf8'),
+              },
+            });
+          }
+          // Respond with a consistent error message
+          // while also hiding api error details
+          let openAIErrorCode = 'N/A';
+          let openAIErrorMessage =
+            'Unfortunately something went wrong, if you would like to report this issue please include the id of this error.';
+
+          const mulesoftErrorMessage = JSON.parse(responseBuffer.toString('utf8'));
+          if (
+            mulesoftErrorMessage.errorDetails[1] &&
+            mulesoftErrorMessage.errorDetails[1].message &&
+            mulesoftErrorMessage.errorDetails[1].message.error &&
+            mulesoftErrorMessage.errorDetails[1].message.error.code &&
+            mulesoftErrorMessage.errorDetails[1].message.error.message
+          ) {
+            openAIErrorCode = mulesoftErrorMessage.errorDetails[1].message.error.code;
+            openAIErrorMessage = mulesoftErrorMessage.errorDetails[1].message.error.message;
+          }
+          clientResponse = createErrorMessage(errorId, openAIErrorCode, openAIErrorMessage);
+        } else {
+          logger.info(`GPT 4 Prompt request successful ${proxyRes.statusCode} `);
+          clientResponse = responseBuffer;
+        }
+
+        return clientResponse;
+      }),
     },
     selfHandleResponse: true,
-    onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
-      let clientResponse;
-
-      if (proxyRes.statusCode !== 200) {
-        const errorId = uuidv4();
-        logger.error(
-          `Chat GPT 4 API Failed... Error Id ${errorId} HTTP Status Code ${
-            proxyRes.statusCode
-          } HTTP Error Message ${responseBuffer.toString('utf8')} `,
-        );
-
-        if (process.env.DEPLOY_ENVIRONMENT === 'cloud') {
-          appInsights.defaultClient.trackTrace({
-            message: 'ChatApp GPT 4 Prompt API Failed',
-            severity: 3, // Error
-            properties: {
-              chatAppErrorId: errorId,
-              httpStatusCode: proxyRes.statusCode,
-              errorMessage: responseBuffer.toString('utf8'),
-            },
-          });
-        }
-        // Respond with a consistent error message
-        // while also hiding api error details
-        let openAIErrorCode = 'N/A';
-        let openAIErrorMessage =
-          'Unfortunately something went wrong, if you would like to report this issue please include the id of this error.';
-
-        const mulesoftErrorMessage = JSON.parse(responseBuffer.toString('utf8'));
-        if (
-          mulesoftErrorMessage.errorDetails[1] &&
-          mulesoftErrorMessage.errorDetails[1].message &&
-          mulesoftErrorMessage.errorDetails[1].message.error &&
-          mulesoftErrorMessage.errorDetails[1].message.error.code &&
-          mulesoftErrorMessage.errorDetails[1].message.error.message
-        ) {
-          openAIErrorCode = mulesoftErrorMessage.errorDetails[1].message.error.code;
-          openAIErrorMessage = mulesoftErrorMessage.errorDetails[1].message.error.message;
-        }
-        clientResponse = createErrorMessage(errorId, openAIErrorCode, openAIErrorMessage);
-      } else {
-        logger.info(`GPT 4 Prompt request successful ${proxyRes.statusCode} `);
-        clientResponse = responseBuffer;
-      }
-
-      return clientResponse;
-    }),
     logger: console,
   });
 
