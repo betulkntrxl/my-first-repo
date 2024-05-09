b141ff288ff2323282d37856a9dfacb253dd5701
Asif28851 <Asif.khan@mckesson.com>
2024-05-09 20:19:03 +0530

    Chat app test case fix

--- src/tests/DownloadConversation.test.tsx
+++ src/tests/DownloadConversation.test.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import axios from 'axios';
 import { render, cleanup, screen, waitFor, fireEvent, act } from '@testing-library/react';
+import { setupMockAxiosSuccessResponses } from './test-helper';
 import DownloadConversation from '../components/DownloadConversation/DownloadConversation';
 import { downloadConversation } from '../components/DownloadConversation/DownloadUtils';
-import { setupMockAxiosSuccessResponses } from './test-helper';
-
+// jest.mock('../components/DownloadConversation/DownloadUtils');
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
@@ -15,9 +15,7 @@ describe('DownloadConversation Tests', () => {
     expect(getByTestId('textIcon')).toBeInTheDocument();
     expect(getByTestId('downloadIcon')).toBeInTheDocument();
     fireEvent.click(getByTestId('textIcon'));
-  });
 
-  it('should download correctly', () => {
     const mLink = {
       href: '',
       click: jest.fn(),
@@ -38,4 +36,26 @@ describe('DownloadConversation Tests', () => {
     expect(mLink.click).toHaveBeenCalled();
     expect(document.body.removeChild).toHaveBeenCalledWith(mLink);
   });
+
+  // it('should download correctly', () => {
+  //     const mLink = {
+  //         href: '',
+  //         click: jest.fn(),
+  //         download: '',
+  //         style: { display: '' },
+  //         setAttribute: jest.fn(),
+  //     } as any;
+  //     const createElementSpy = jest.spyOn(document, 'createElement').mockReturnValueOnce(mLink);
+  //     document.body.appendChild = jest.fn();
+  //     document.body.removeChild = jest.fn();
+  //     downloadConversation('blobUrl', 'go');
+  //     expect(createElementSpy).toHaveBeenCalledWith('a');
+  //     expect(mLink.setAttribute.mock.calls.length).toBe(2);
+  //     expect(mLink.setAttribute.mock.calls[0]).toEqual(['href', 'data:text/plain;charset=utf-8,go']);
+  //     expect(mLink.setAttribute.mock.calls[1]).toEqual(['download', 'blobUrl']);
+  //     expect(mLink.style.display).toBe('none');
+  //     expect(document.body.appendChild).toHaveBeenCalledWith(mLink);
+  //     expect(mLink.click).toHaveBeenCalled();
+  //     expect(document.body.removeChild).toHaveBeenCalledWith(mLink);
+  // });
 });
